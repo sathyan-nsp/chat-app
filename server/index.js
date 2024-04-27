628d4d424c70654861a50ddda1803cf9ad9759fe
Sathyan <sathyannsp@gmail.com>
2024-04-27 20:52:36 +0530

    chore: initial commit

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const userRoute = require("./Routes/userRoute");
+
+const app = express();
+require("dotenv").config();
+
+app.use(express.json());
+app.use(cors());
+app.use("/api/users", userRoute);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to our chat APIs");
+});
+
+app.post("/", (req, res) => {});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, (req, res) => {
+  console.log(`Server running on port ${port}`);
+});
+
+mongoose
+  .connect(process.env.ATLAS_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Connection established"))
+  .catch((error) => console.log("MongoDB Connection Failed: ", error.message));
